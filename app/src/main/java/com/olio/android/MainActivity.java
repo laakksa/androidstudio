f003802a007a1da1466dcf7f9ed3b24ec7d8d035
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-02-23 15:48:12 +0200

    v7t3

--- app/src/main/java/com/olio/android/MainActivity.java
+++ app/src/main/java/com/olio/android/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends Activity {
     TextView text;
+    EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +20,12 @@ public class MainActivity extends Activity {
 
 
     public void helloWorld(View v){
+        Scanner scan = new Scanner(System.in);
         text = (TextView) findViewById(R.id.textView);
-        text.setText("Hello World!");
+        edit = (EditText) findViewById(R.id.input);
+        String input = edit.getText().toString();
+        text.setText(input);
+
     }
+
 }
