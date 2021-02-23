6599fbb031c7df9cc3eb7797d141390777f6245d
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-02-23 16:07:00 +0200

    v7t4

--- app/src/main/java/com/olio/android/MainActivity.java
+++ app/src/main/java/com/olio/android/MainActivity.java
@@ -3,11 +3,11 @@ package com.olio.android;
 import android.app.Activity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.Scanner;
 
 public class MainActivity extends Activity {
     TextView text;
@@ -16,16 +16,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    public void helloWorld(View v){
-        Scanner scan = new Scanner(System.in);
         text = (TextView) findViewById(R.id.textView);
         edit = (EditText) findViewById(R.id.input);
-        String input = edit.getText().toString();
-        text.setText(input);
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
+
 
 }
