c1c16e34059e73c7ab8075453c802abef497b2be
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-03-03 18:27:16 +0200

    v7t5

--- app/src/main/java/com/olio/android/MainActivity.java
+++ app/src/main/java/com/olio/android/MainActivity.java
@@ -2,40 +2,58 @@ package com.olio.android;
 
 import android.app.Activity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends Activity {
-    TextView text;
-    EditText edit;
+    EditText input, fileinput;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textView);
-        edit = (EditText) findViewById(R.id.input);
-        edit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        input = (EditText) findViewById(R.id.input);
+        fileinput = (EditText) findViewById(R.id.fileinput);
+        context = MainActivity.this;
+        System.out.println("Files location: " + context.getFilesDir());
+        }
 
+
+    public void readFile(View v){
+        try{
+            InputStream is = context.openFileInput(fileinput.getText().toString());
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String temp;
+            input.setText("");
+            while ((temp = br.readLine()) != null){
+                input.append(temp);
             }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                text.setText(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
+            is.close();
+        } catch (IOException e){
+            Log.e("IOException", "Faulty input.");
+        }
     }
 
+    public void writeFile(View v){
+        try{
+            OutputStreamWriter osw = new OutputStreamWriter(context.openFileOutput(fileinput.getText().toString(), Context.MODE_PRIVATE));
+            String temp = input.getText().toString();
+            osw.write(temp);
+            osw.close();
+        } catch (IOException e){
+            Log.e("IOException", "Faulty input.");
+        } finally{
+            input.setText("");
+        }
+    }
 
 }
